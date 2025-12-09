f8471cfadaf97fdc0595abce9b537fc9cccc49d2
Hyoju <163081966+Hyoso0w0@users.noreply.github.com>
2025-12-10 00:54:23 +0900

    레벨 수치 수정

--- data/levels.js
+++ data/levels.js
@@ -1,38 +1,23 @@
 export const levelStages = {
-    waste: [
-      { id: 1, target: 5, result: '바다거북 1마리 구하기', property: "waste" },
-      { id: 2, target: 10, result: '플라스틱 빨대 2,000개 줄이기', property: "waste" },
-      { id: 3, target: 15, result: '해양 쓰레기 15kg 제거하기', property: "waste" },
-      { id: 4, target: 22, result: '비닐봉지 500개 줄이기', property: "waste" },
-      { id: 5, target: 30, result: '일회용품 없는 카페 1달 운영', property: "waste" },
-      { id: 6, target: 40, result: '플라스틱 섬 면적 1㎡ 줄이기', property: "waste" },
-      { id: 7, target: 52, result: '산호초 1개 군락 보호하기', property: "waste" },
-      { id: 8, target: 66, result: '바다새 100마리 구하기', property: "waste" },
-      { id: 9, target: 82, result: '해양 생태계 1개 구역 복원', property: "waste" },
-      { id: 10, target: 100, result: '무플라스틱 해변 1km 만들기', property: "waste" }
-    ],
-    water: [
-      { id: 1, target: 5000, result: '아프리카 어린이 1명 1주일 식수', property: "water" },
-      { id: 2, target: 10000, result: '물부족 가정 1달 수량 지원', property: "water" },
-      { id: 3, target: 16000, result: '사막화 지역 묘목 10그루 심기', property: "water" },
-      { id: 4, target: 23000, result: '건조지역 우물 1개 채우기', property: "water" },
-      { id: 5, target: 31000, result: '물부족 학교 1주일 급수 지원', property: "water" },
-      { id: 6, target: 40000, result: '가뭄 지역 농작물 재배 돕기', property: "water" },
-      { id: 7, target: 50000, result: '물부족 마을 1개월 식수 지원', property: "water" },
-      { id: 8, target: 61000, result: '습지 생태계 1㎢ 복원하기', property: "water" },
-      { id: 9, target: 73000, result: '물부족 국가 가족 1년 식수량', property: "water" },
-      { id: 10, target: 86000, result: '깨끗한 강 1km 되살리기', property: "water" }
-    ],
-    carbon: [
-      { id: 1, target: 1500, result: '도시 미세먼지 1시간분 줄이기', property: "carbon" },
-      { id: 2, target: 3000, result: '나무 3그루 1년 흡수량 달성', property: "carbon" },
-      { id: 3, target: 4800, result: '승용차 200km 운행량 줄이기', property: "carbon" },
-      { id: 4, target: 6900, result: '북극 빙하 1㎡ 지키기', property: "carbon" },
-      { id: 5, target: 9300, result: '북극곰 서식지 10㎡ 보호하기', property: "carbon" },
-      { id: 6, target: 12000, result: '열대우림 100㎡ 지키기', property: "carbon" },
-      { id: 7, target: 15000, result: '북극곰 1마리 빙하 발판 만들기', property: "carbon" },
-      { id: 8, target: 18300, result: '온실가스 가정 1개월분 감축', property: "carbon" },
-      { id: 9, target: 21900, result: '북극 해빙 면적 50㎡ 보존', property: "carbon" },
-      { id: 10, target: 25800, result: '탄소중립 숲 1개 조성하기', property: "carbon" }
-    ]
-  };
+  waste: [
+    { id: 1, target: 500, result: '쓰레기차에서 종량제 봉투 100개 덜어내기', property: "waste" },
+    { id: 2, target: 2000, result: '생활폐기물 배출량 감쇄', property: "waste" },
+    { id: 3, target: 5000, result: '해변가의 쓰레기를 깨끗하게', property: "waste" },
+    { id: 4, target: 10000, result: '중학생 한 명 만큼 깨끗해지는 지구', property: "waste" },
+    { id: 5, target: 20000, result: '배달 쓰레기 폭격 막아내기', property: "waste" },
+  ],
+  water: [
+    { id: 1, target: 0.5, result: '베란다 화분에 생명을', property: "water" },
+    { id: 2, target: 5, result: '유기견 50마리 하루 식수 지원 ', property: "water" },
+    { id: 3, target: 20, result: '가뭄 속 작은 텃밭 살리기', property: "water" },
+    { id: 4, target: 60, result: '4인 가족의 한 달 식수 안전망 확보', property: "water" },
+    { id: 5, target: 100, result: '초기 산불 진압의 영웅', property: "water" },
+  ],
+  carbon: [
+    { id: 1, target: 2000, result: '출근길 도로 위 배기가스 제거', property: "carbon" },
+    { id: 2, target: 10000, result: '주말에 다녀온 드라이브 매연 무효화', property: "carbon" },
+    { id: 3, target: 30000, result: '소나무 5그루 심기', property: "carbon" },
+    { id: 4, target: 70000, result: '내 휴대폰 제조 탄소발자국 상쇄', property: "carbon" },
+    { id: 5, target: 150000, result: '제주도 왕복 비행기 탄소 차단의 수호자', property: "carbon" },
+  ]
+};
